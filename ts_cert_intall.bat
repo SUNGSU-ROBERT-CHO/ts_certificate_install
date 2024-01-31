2030d65a3991e01b8c052a8f81f7aff0c99470d1
Robert Cho <sscho@crosscert.com>
2024-01-31 15:34:21 +0900

    # WINDOWS 관리자 권한으로 실행시 버그 수정 - 관리자 권한으로 실행시 system32 폴더로 가는 현상 수정 - keytool 에러시 멈춤없이 종료 되는 현상 수정

--- ts_cert_intall.bat
+++ ts_cert_intall.bat
@@ -2,14 +2,14 @@
 
 set ROOTCA_CERT="./cert/OISTE WISeKey Global Root GB CA.pem"
 
-set ROOTCA_ALIAS1="oiste_wisekey_global_root_gb_ca"
-set ROOTCA_ALIAS2="oistewisekeyglobalrootgbca"
+set ROOTCA_ALIAS1=oiste_wisekey_global_root_gb_ca
+set ROOTCA_ALIAS2=oistewisekeyglobalrootgbca
 set CACERTS_PASSWORD=changeit
 
 set RETURN_VALUE=
 set TEMP_FILE=temp_output.txt
-set TEMP_FILE2=temp_output2.txt
 
+cd %~dp0 
 
 CALL :IsExistRootCA %ROOTCA_ALIAS1%
 IF ["%RETURN_VALUE%"]==[""] (
@@ -17,18 +17,28 @@ IF ["%RETURN_VALUE%"]==[""] (
 )
 
 IF ["%RETURN_VALUE%"]==[""] (
+    echo Import %ROOTCA_ALIAS1%
     keytool -import -cacerts -noprompt -file %ROOTCA_CERT% -alias %ROOTCA_ALIAS1% -storepass %CACERTS_PASSWORD%
-    echo Import RootCA : %ROOTCA_ALIAS1%
-    goto :eof 
+) else (
+    echo Already Included RootCA : %RETURN_VALUE%
 )
 
-echo Already Included RootCA : %RETURN_VALUE%
 pause
 
 :: Check Root Certificate
 :IsExistRootCA
-call keytool -list -cacerts -storepass %CACERTS_PASSWORD% > %TEMP_FILE%
-call findstr /i /c:"%ROOTCA_ALIAS1%" %TEMP_FILE% > %TEMP_FILE2%
-set /p RETURN_VALUE=<%TEMP_FILE2%
-del %TEMP_FILE% %TEMP_FILE2%
+keytool -list -cacerts -storepass %CACERTS_PASSWORD% > %TEMP_FILE%
+if %ERRORLEVEL% neq 0 (
+    echo Error: Not found keytool.
+    del %TEMP_FILE%
+    pause
+    exit
+)
+
+findstr /i /c:"%~1" %TEMP_FILE%
+if %ERRORLEVEL% equ 0 (
+    set RETURN_VALUE=%~1
+)
+
+del %TEMP_FILE%
 EXIT /B 0
